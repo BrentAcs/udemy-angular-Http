e032b5102890e3ebd5f262d8dd4ee1e873920d5a
Brent Acs <brent.mann@gmail.com>
2021-02-22 08:20:30 -0500

    EoL 18.257 added rxjs map to pipe get response and transform it for the subscribe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: "app-root",
@@ -42,6 +43,17 @@ export class AppComponent implements OnInit {
       .get(
         "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json"
       )
+      .pipe(map(
+        (responseData) => {
+          const postsArray = [];
+          for( const key in responseData ){
+            if(responseData.hasOwnProperty(key)){
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }
+      ))
       .subscribe((posts) => {
         console.log("fetchPosts: ");
         console.log(posts);
