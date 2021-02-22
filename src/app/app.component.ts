f990b2233235c6b9dc1d38d9472cd6cbe7729581
Brent Acs <brent.mann@gmail.com>
2021-02-21 21:26:18 -0500

    EoL 18.256 added fetch (get) for posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,23 +13,38 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http.post(
-      "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json",
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.http
+      .post(
+        "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log("onCreatePost: " + responseData);
+      });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json"
+      )
+      .subscribe((posts) => {
+        console.log("fetchPosts: ");
+        console.log(posts);
+      });
   }
 }
