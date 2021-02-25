f7a3fb23287ebd2526c0c8a164267d1d7e1409dc
Brent Acs <brent.mann@gmail.com>
2021-02-25 13:11:53 -0500

    EoL 18.269 demo of  adding query params to request.

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -29,11 +29,17 @@ export class PostsService {
   }
 
   fetchPosts() {
+    // demo how to send multiple params 
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("custom", "key");
     return this.http
       .get<{ [key: string]: Post }>(
         "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json",
         {
-          headers: new HttpHeaders({"Custom-Header": "Hello" })
+          headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+          // params: new HttpParams().set('print', 'pretty')
+          params: searchParams,
         }
       )
       .pipe(
