aa4e1828b9c098af5bc00f95344cbb81c3f8d988
Brent Acs <brent.mann@gmail.com>
2021-02-25 08:58:25 -0500

    EoL 18.261 migrated http requests to a PostsService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
 import { Post } from "./post.model";
+import { PostsService } from "./posts.service";
 
 @Component({
   selector: "app-root",
@@ -15,54 +16,23 @@ export class AppComponent implements OnInit {
 
   // Firebase:  https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.onFetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http
-      .post<{name: string}>(
-        "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json",
-        postData
-      )
-      .subscribe((responseData) => {
-        console.log("onCreatePost: " + responseData);
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts() {
-    this.isFetching = false;
-    this.http
-      .get<{ [key: string]: Post }>(
-        "https://ng-complete-guide-4e0a0-default-rtdb.firebaseio.com/posts.json"
-      )
-      .pipe(
-        map((responseData) => {
-          const postsArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postsArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return postsArray;
-        })
-      )
-      .subscribe((posts) => {
-        this.isFetching = false;
-        console.log("fetchPosts: ");
-        console.log(posts);
-        this.loadedPosts = posts;
-      });
-  }
 }
