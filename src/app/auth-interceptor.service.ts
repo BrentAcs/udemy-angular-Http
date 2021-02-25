a0615371028ff2d21622fbdf7860aa316aa62f43
Brent Acs <brent.mann@gmail.com>
2021-02-25 16:54:57 -0500

    EoL 18.273 added fake 'Auth' header in Http interceptor.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -9,6 +9,13 @@ import { Observable } from "rxjs";
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log("Request is on its way");
-    return next.handle(req);
+    console.log(req);
+    const modifiedRequest = req.clone(
+      {
+        headers: req.headers.append('Auth', 'whodaboss'),
+      }
+    );
+
+    return next.handle(modifiedRequest);
   }
 }
