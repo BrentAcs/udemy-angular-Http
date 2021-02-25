c5a5a5ddd1bd34820cfba13072ad7197a63ffa33
Brent Acs <brent.mann@gmail.com>
2021-02-25 11:27:51 -0500

    added catchError rxjs operator.

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 import { Post } from "./post.model";
 
@@ -42,6 +42,10 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError((errorResult) => {
+          // send to analytics/logger service
+          return throwError(errorResult);
         })
       );
   }
